70ef216827b6d0e8b6dfa24645d3f3ee365cf86d
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2017-03-17 21:55:14 +1300

    SilverStripe CMS requirements installer.

--- installers/ubuntu/16.4/silverstripe-requirements.sh
+++ installers/ubuntu/16.4/silverstripe-requirements.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo apt install php5.6-gd php5.6-mysql php5.6-pgsql php5.6-curl php5.6-cli php5.6-dom php5.6-mbstring
+sudo apt install php5.6-gd php5.6-mysql php5.6-pgsql php5.6-curl php5.6-cli php5.6-dom php5.6-mbstring php5.6-tidy mysql-server mysql-client
+chmod -R a+w .htaccess mysite/_config.php assets/
+
