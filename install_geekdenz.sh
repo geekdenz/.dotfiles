bf136164262857895a88dbd89236f83db86acbca
Tim-Hinnerk Heuer <heuert@landcareresearch.co.nz>
2020-08-12 21:38:14 +1200

    improve vscode and powerline support

--- install_geekdenz.sh
+++ install_geekdenz.sh
@@ -5,3 +5,17 @@
 curl https://raw.githubusercontent.com/geekdenz/.dotfiles/master/install.sh | bash
 
 ln -sf ~/.dotfiles/git/gitconfig.local ~/.gitconfig.local
+ln -sf ~/.dotfiles/shells/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+# ensure powerline is installed
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+# install Hack font
+curl https://www.fontsquirrel.com/fonts/download/hack > /tmp/hack.zip && \
+	pushd /tmp && \
+	unzip hack.zip && \
+	mkdir ~/.local/share/fonts -p && mv Hack*.ttf ~/.local/share/fonts && \
+	rm -f hack.zip && \
+	popd
+
+curl -s "https://get.sdkman.io" | bash
+
