ae94ba1c30b4258f82f05e57beea0c23bba83d97
Tim-Hinnerk Heuer <heuert@landcareresearch.co.nz>
2020-08-12 23:56:23 +1200

    improve shell, modify vscode settings

--- install_geekdenz.sh
+++ install_geekdenz.sh
@@ -7,6 +7,8 @@ curl https://raw.githubusercontent.com/geekdenz/.dotfiles/master/install.sh | ba
 ln -sf ~/.dotfiles/git/gitconfig.local ~/.gitconfig.local
 ln -sf ~/.dotfiles/shells/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+ln -sf ~/.dotfiles/shells/ssh-add.sh ~/.config/autostart-scripts/ssh-add.sh
+mkdir -p ~/.config/plasma-workspace/env && ln -sf ~/.dotfiles/shells/ask-pass.sh ~/.config/plasma-workspace/env/askpass.sh
 # ensure powerline is installed
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 # install Hack font
