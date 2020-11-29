be49b2ab8857028f23533b7152cfb29445d945e5
Tim-Hinnerk Heuer <heuert@landcareresearch.co.nz>
2020-11-29 19:11:16 +1300

    update installers and make install_geekdenz.sh work

--- installers/ubuntu/16.4/nvm.sh
+++ installers/ubuntu/16.4/nvm.sh
@@ -6,5 +6,5 @@ source "`dirname $0`/config.sh"
 
 set -e
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 nvm install --lts
