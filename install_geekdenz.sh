737dc05f004428a46f781f117832f19edbd49ab1
Tim-Hinnerk Heuer <heuert@landcareresearch.co.nz>
2020-11-29 20:53:07 +1300

    add ssh keys on kde login

--- install_geekdenz.sh
+++ install_geekdenz.sh
@@ -4,6 +4,9 @@ sudo apt update && sudo apt install -y zsh vim-gtk git-all && \
 ./install.sh
 ./installers/ubuntu/16.4/nvm.sh
 nvm install --lts
+mkdir -p $HOME/.config/autostart-scripts
+ln -sf ~/.dotfiles/shells/ssh-unlock.sh $HOME/.config/autostart-scripts/ssh-unlock.sh
+#cp ~/.dotfiles/shells/ssh-unlock.sh $HOME/.config/autostart-scripts/ssh-unlock.sh
 mkdir -p ~/.config/plasma-workspace/env && ln -sf ~/.dotfiles/shells/ask-pass.sh ~/.config/plasma-workspace/env/askpass.sh
 # ensure powerline is installed
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
---
+++ shells/ssh-unlock.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+SSH_ASKPASS=/usr/bin/ksshaskpass ssh-add $HOME/.ssh/id_rsa $HOME/.ssh/id_thheuer </dev/null
