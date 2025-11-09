3ea0ec0319727963be0f86e408d4f418cbeedc37
Tim-Hinnerk Heuer <geekdenz@@users.noreply.github.com>
2025-11-09 20:51:10 +1300

    WIP: halving plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- For init.lua
+vim.o.guifont = "JetBrainsMono Nerd Font:h12" -- Adjust font size as needed
+-- set guifont=JetBrainsMono\ Nerd\ Font\ Mono:h12
