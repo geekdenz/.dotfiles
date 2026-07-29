8932d8c6b5fc07ad301cfce00812e74befc96a15
Tim-Hinnerk Heuer <geekdenz@@users.noreply.github.com>
2026-07-29 21:44:49 +1200

    feat(terminal): add version managers for nvim and wezterm

---
+++ nvim/after/ftplugin/cpp.lua
@@ -0,0 +1,9 @@
+-- Keep C++ indentation local and handled by Vim's built-in C/C++ indenter.
+vim.opt_local.cindent = true
+vim.opt_local.indentexpr = ""
+vim.opt_local.smartindent = false
+
+vim.opt_local.expandtab = true
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.tabstop = 4
